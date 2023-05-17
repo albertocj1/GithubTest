abca7939682e8ca9c91c6b81066570997789ea95
albertocj1 <albertochristianjoshua@gmail.com>
2023-05-17 22:17:28 +0800

    Rebuilt the whole code
    added javaFx and create a switch scene function

--- src/App.java
+++ src/App.java
@@ -1,7 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("hello team!");
-        System.out.println("Hello my team!");
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+
+    public static void main(String[] args) {
+        
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception{
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));  
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
